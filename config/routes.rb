a233a13a57118cc0d3b7662e2ad148d3b27bfb9b
Ved Tiwari <ved@webkorps.com>
2022-09-16 21:55:29 +0530

    create controller ,model and migration ,and update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'employees/index'
+  get 'employees/show'
+  get 'employees/nwe'
+  get 'employees/create'
+  get 'employees/edit'
+  get 'employees/update'
+  get 'employees/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
