266861abd44c80da558170eb1ef36e7bd0982b7b
Ved Tiwari <ved@webkorps.com>
2022-09-17 02:35:13 +0530

    add validations and before action

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,11 +1,11 @@
 class EmployeesController < ApplicationController
+  before_action :set_emp, only: [:edit, :update, :show, :destroy]
 
     def index
       @emp=Employee.all
     end
 
     def show
-      @emp = Employee.find(params[:id])
     end
 
     def new
@@ -13,7 +13,7 @@ class EmployeesController < ApplicationController
     end
 
     def create
-      @emp = Employee.new(params.require(:employee).permit(:first_name, :middle_name, :last_name, :personal_email,:city, :state, :country, :pincode, :address_1,:address_2))
+      @emp = Employee.new(emp_params)
       if @emp.save
         flash[:notice] = "Employee was created successfully."
         redirect_to employees_path
@@ -23,22 +23,30 @@ class EmployeesController < ApplicationController
     end
 
     def edit
-      @emp = Employee.find(params[:id])
     end
 
     def update
-      @emp  = Employee.find(params[:id])
-          if @emp.update(params.require(:employee).permit(:first_name, :middle_name, :last_name, :personal_email,:city, :state, :country, :pincode, :address_1,:address_2))
-            flash[:notice] = "Employee was updated successfully."
-            redirect_to employees_path
-          else
-            render 'edit', status: :unprocessable_entity
-          end
-        end
+      if @emp.update(emp_params)
+        flash[:notice] = "Employee was updated successfully."
+          redirect_to employees_path
+      else
+          render 'edit', status: :unprocessable_entity
+      end   
     end
 
     def destroy
-      @emp = Employee.find(params[:id])
       @emp.destroy
       redirect_to employees_path
     end
+
+    private
+
+    def emp_params
+      params.require(:employee).permit(:first_name, :middle_name, :last_name, :personal_email,:city, :state, :country, :pincode, :address_1,:address_2)
+    end
+
+    def set_emp
+      @emp = Employee.find(params[:id])
+    end
+      
+  end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,23 @@
 class Employee < ApplicationRecord
+    validates :first_name ,:last_name, presence: true,  
+    length: { minimum: 2, maximum: 15}
+
+    validates :personal_email, presence: true, 
+    uniqueness: { case_sensitive: false }, 
+    length: {minimum: 5, maximum:30 }
+    
+    validates :city, :state, :pincode ,:country,:address_1,:address_2 , presence: true, 
+     
+    length: {minimum: 2, maximum: 20}
+
+
+
+    def name
+        "#{first_name} #{last_name}".strip
+    end
+
+    def full_address
+        "#{address_1} #{address_2} ,#{city} ,#{state},#{country}, #{pincode}"
+    end
+
 end
