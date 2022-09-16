1b6fd52a26c311d4a47c77077f8b28093070a373
Ved Tiwari <ved@webkorps.com>
2022-09-17 01:20:45 +0530

    create controller index,create, edit ,show, update, destroy ,new action and create employee form, update employee form, index and show page and add routes

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,22 +1,44 @@
 class EmployeesController < ApplicationController
-  def index
-  end
 
-  def show
-  end
+    def index
+      @emp=Employee.all
+    end
 
-  def nwe
-  end
+    def show
+      @emp = Employee.find(params[:id])
+    end
 
-  def create
-  end
+    def new
+      @emp= Employee.new
+    end
 
-  def edit
-  end
+    def create
+      @emp = Employee.new(params.require(:employee).permit(:first_name, :middle_name, :last_name, :personal_email,:city, :state, :country, :pincode, :address_1,:address_2))
+      if @emp.save
+        flash[:notice] = "Employee was created successfully."
+        redirect_to employees_path
+      else
+        render 'new', status: :unprocessable_entity
+      end  
+    end
 
-  def update
-  end
+    def edit
+      @emp = Employee.find(params[:id])
+    end
 
-  def destroy
-  end
-end
+    def update
+      @emp  = Employee.find(params[:id])
+          if @emp.update(params.require(:employee).permit(:first_name, :middle_name, :last_name, :personal_email,:city, :state, :country, :pincode, :address_1,:address_2))
+            flash[:notice] = "Employee was updated successfully."
+            redirect_to employees_path
+          else
+            render 'edit', status: :unprocessable_entity
+          end
+        end
+    end
+
+    def destroy
+      @emp = Employee.find(params[:id])
+      @emp.destroy
+      redirect_to employees_path
+    end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  get 'employees/index'
-  get 'employees/show'
-  get 'employees/nwe'
-  get 'employees/create'
-  get 'employees/edit'
-  get 'employees/update'
-  get 'employees/destroy'
+  # get 'employees/index'
+  # get 'employees/show'
+  # get 'employees/new'
+  # get 'employees/create'
+  # get 'employees/edit'
+  # get 'employees/update'
+  # get 'employees/destroy'
+  resources :employees
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
